b854b83108914c914e28ef03eeb05616f4316c09
Zhenchun Li <zhenchun.li@kunlun-inc.com>
2025-12-11 22:13:57 +0800

    update example

--- example/flac-exam.go
+++ example/flac-exam.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
-	"os"
-
 	"github.com/lizc2003/audiocodec"
+	"os"
 )
 
 func main() {
+	flacToWavFile()
+	flacToWavMemory()
+}
+
+func flacToWavFile() {
 	inFile, err := os.Open("samples/sample.flac")
 	if err != nil {
 		fmt.Println(err)
@@ -28,5 +34,29 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Decoded %d bytes, totalSamples: %d, sampleRate: %d\n", totalBytes, totalSamples, sampleRate)
+	fmt.Printf("Write to File, Decoded %d bytes, totalSamples: %d, sampleRate: %d\n", totalBytes, totalSamples, sampleRate)
+}
+
+func flacToWavMemory() {
+	inFile, err := os.Open("samples/sample.flac")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer inFile.Close()
+
+	writer := &audiocodec.WriterSeeker{}
+	totalBytes, totalSamples, sampleRate, err := audiocodec.FlacToWav(inFile, writer)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	bytes := writer.Bytes()
+	md5sum := md5.Sum(bytes[audiocodec.WavHeaderSize:])
+	if hex.EncodeToString(md5sum[:]) != "372fd5b0a07a0a78b92311ccdca4cc81" {
+		fmt.Println("md5sum check wrong")
+		return
+	}
+
+	fmt.Printf("Write to Memory, Decoded %d bytes, totalSamples: %d, sampleRate: %d\n", totalBytes, totalSamples, sampleRate)
 }
